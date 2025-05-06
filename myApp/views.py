9ed5e9d7ffd10f84664b26436592161cadd9464b
Mithun <sbmithun@gmail.com>
2025-05-06 16:34:48 +0900

    basic setup

--- myApp/views.py
+++ myApp/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
-# Create your views here.
+from django.shortcuts import render, redirect  
+from django.contrib import messages
+
+from django.http import JsonResponse
+import json
+from django.views.decorators.csrf import csrf_exempt
+
+from django.contrib.auth.decorators import login_required
+
+
+def index(request):
+    return render(request, "myApp/index.html")
+
