70b632e47124097487eea3beb4dfc5c64cb669c2
Mithun <sbmithun@gmail.com>
2025-05-06 20:13:45 +0900

    base func tweaked and final

--- myApp/urls.py
+++ myApp/urls.py
@@ -6,6 +6,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("login_view", views.login_view, name="login_view"),
+    path("register", views.register, name="register"),
+    path("logout_view", views.logout_view, name="logout_view"),    
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
