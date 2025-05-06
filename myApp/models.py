bead73ef4e3fef741f9a9bbc3e77016626755f8c
Mithun <sbmithun@gmail.com>
2025-05-06 19:47:45 +0900

    base website complete

--- myApp/models.py
+++ myApp/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models 
 
-# Create your models here.
+#gives field types like CharField, IntegerField, TextField, ImageField
+#relatoinships like OneToOneField, ForeignKey, ManyToManyField
+
+
+class User(AbstractUser):
+    
+    def __str__(self):
+        return f"{self.username}, ({self.email})"
+
