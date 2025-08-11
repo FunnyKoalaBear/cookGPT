f80e4059d7de19b09733d026bba39ad5bd28eb79
FunnyKoalaBear <sbmithun@gmail.com>
2025-08-12 01:37:33 +0900

    js loaded succesfully

--- cookGPT/settings.py
+++ cookGPT/settings.py
@@ -116,7 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -125,9 +124,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
 
 
 AUTH_USER_MODEL = 'myApp.User'
